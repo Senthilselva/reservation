6374a98c9b45834782a17428a95fb1683ab6dfbe
jj99999 <jonathan.jackson3@gmail.com>
2016-10-29 12:47:20 -0400

    added jQuery and dynamic HTML for pushing array data into tables

--- express.js
+++ express.js
@@ -28,10 +28,10 @@ app.get('/table', function (req, res) {
 	res.sendFile(path.join(__dirname, 'table.html'));
 });
 
-app.get('/reserver' function(req,res){
-	res.sendFile(path.join(__dirname, 'reserver.html'))
+app.get('/reserve', function(req,res){
+	res.sendFile(path.join(__dirname, 'reserve.html'))
 
-})
+});
 
 
 
